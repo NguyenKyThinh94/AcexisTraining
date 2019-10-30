5bb48b219fb23f0d2d95119b2789f5b8522c1fdb
= <=>
2019-10-30 16:39:53 +0700

    Done

--- 4_Array Cardio Day/index.js
+++ 4_Array Cardio Day/index.js
@@ -1,7 +1,7 @@
 let productList=  [
     {itermId: "1", productName: "Bong Den", actualPrice: "53000", discountPrice: "42000", stockAvailable: "111" },
     {itermId: "2", productName: "Day dien", actualPrice: "35000", discountPrice: "35000", stockAvailable: "2" },
-    {itermId: "3", productName: "Cong tac", actualPrice: "20000", discountPrice: "1900", stockAvailable: "15" },
+    {itermId: "3", productName: "Cong tac", actualPrice: "20000", discountPrice: "1000", stockAvailable: "13" },
     {itermId: "4", productName: "O cam dien", actualPrice: "50000", discountPrice: "43000", stockAvailable: "0" },
     {itermId: "5", productName: "Noi com dien", actualPrice: "20000", discountPrice: "17000", stockAvailable: "31" },
     {itermId: "6", productName: "Quat hoi nuoc", actualPrice: "1000000", discountPrice: "970000", stockAvailable: "0" },
@@ -14,18 +14,37 @@ function renderHeaderTable(array){
     let headerTable=document.querySelector('thead');
     headerTable.innerHTML='';
     let listKey=Object.keys(array[0]);
-    let listKeyStr=listKey.map((element) => "<th data-checked='1'>"+element+"<th>");
-    headerTable.innerHTML= "<tr>" + listKeyStr.join('')+ "</tr>";
+    listKey=listKey.map((element) => "<th data-checked='1'id='"+element+"'>"+element+"<span class='tooltiptext'>Click to sort</span></th>");
+    let listKeyStr=listKey.join('');
+    console.log("<tr>" + listKeyStr+ "</tr>");
+    
+    headerTable.innerHTML= "<tr>" + listKeyStr+ "</tr>";
+}
+function calculateTotal(array){
+    let totalActua= array.reduce(function(pre,cur){
+        return pre + parseInt(cur.actualPrice)*parseInt(cur.stockAvailable);
+    },0);
+    let totalaDiscount= array.reduce(function(pre,cur){
+        return pre + parseInt(cur.discountPrice)*parseInt(cur.stockAvailable);
+    },0);
+
+    return "<tr>"
+             +"<td colspan='2'>Tổng giá trị bằng(Price*Stock):</td>"
+             +"<td>"+totalActua.toString()+"</td>"
+             +"<td>"+totalaDiscount.toString()+"</td>"
+             +"<td></td>"
+             +"</tr>";
+
 }
 function renderBodyTable(array){
     let bodyTable=document.querySelector('tbody');
     bodyTable.innerHTML='';
     rowArray=array.map(function(elementRow){
         let RowArr=Object.values(elementRow);
-        let RowArrStr=RowArr.map((element) => "<td>"+element+"<td>");
+        let RowArrStr=RowArr.map((element) => "<td>"+element+"</td>");
         return "<tr>" + RowArrStr.join('')+ "</tr>"
     });
-    bodyTable.innerHTML=rowArray.join('');
+    bodyTable.innerHTML=rowArray.join('')+calculateTotal(array);
    
 }
 function sortnumber(checked,colum){
@@ -78,7 +97,7 @@ function sortLeter(checked,colum){
 }
 function sortList(){
     let checked=this.dataset.checked=='1';
-    let colum=this.innerHTML;
+    let colum=this.id;
     if(colum !== 'productName'){
         this.dataset.checked= sortnumber(checked,colum);
     } else{
