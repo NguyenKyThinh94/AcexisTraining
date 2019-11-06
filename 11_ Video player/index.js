76b588c7c1b11d489117fbd0cef323aa8114fc7b
= <=>
2019-11-06 17:04:30 +0700

    done

--- 11_ Video player/index.js
+++ 11_ Video player/index.js
@@ -2,12 +2,13 @@ let video = document.querySelector('video');
 let previous25sBT=document.getElementById('previous25s');
 let playButton=document.getElementById('playButton');
 let next25sBT=document.getElementById('next25s');
-let playerBar=document.querySelector('#playerBar hoverPlayer');
-let volumeRanger=document.querySelector('#volumeRanger hoverPlayer');
-let root=document.querySelector(':root');
+let playerBar=document.getElementById('playerBar');
+let volumeBar=document.getElementById('volumeBar');
+let speedSelect=document.getElementById('speedSelect');
 
-
-
+function changeRangeInput(obj){
+    obj.style.setProperty('background','linear-gradient(to left, #eee, #eee '+(100-obj.value)+'%, yellow '+(100-obj.value)+'%)'); 
+}
 function playVideo(){
     if(video.paused){
         video.play();
@@ -19,14 +20,37 @@ function playVideo(){
     } 
 }
 function handelBar(){
-    let ranger=(video.currentTime/video.duration).toFixed(4)*100;
-    root.style.setProperty('--playBar',ranger.toString()+'%');
+    let ranger=(video.currentTime/video.duration).toFixed(2);
+    ranger=ranger*100;
+    playerBar.value=ranger;
+    if(ranger>=100){
+        video.pause();
+        playButton.innerHTML='<i class="fas fa-play"></i>';
+    }
+    changeRangeInput(playerBar);
+    
 }
 function next25s(){
     video.currentTime+=25;
+    
 }
 function previous25s(){
     video.currentTime-=25;
+    
+}
+
+function changeTime(){
+    video.currentTime=(playerBar.value*video.duration/100).toFixed(0);
+    changeRangeInput(playerBar);
+}
+function changeVolume(){
+    
+    video.volume=(volumeBar.value*1/100).toFixed(1);
+    changeRangeInput(volumeBar);
+    console.log(video.volume);
+}
+function changeSpeed(){
+    video.playbackRate = parseFloat(speedSelect.value);
 }
 
 video.ontimeupdate=handelBar;
@@ -34,3 +58,8 @@ video.addEventListener('play',handelBar);
 playButton.addEventListener('click', playVideo);
 next25sBT.addEventListener('click',next25s);
 previous25sBT.addEventListener('click',previous25s);
+playerBar.addEventListener('change',changeTime);
+playerBar.addEventListener('mousedown',()=> video.pause());
+playerBar.addEventListener('mouseup',()=> video.play());
+volumeBar.addEventListener('change',changeVolume);
+speedSelect.addEventListener('change',changeSpeed);
